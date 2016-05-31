478791ccfd76c22e269367a02ac975d51eb7f6fc
vladinir <metenevvv@gmail.com>
2016-05-31 23:50:15 +0500

    delete useless code

--- src/main/java/GVisionQuickstart.java
+++ src/main/java/GVisionQuickstart.java
@@ -1,13 +1,14 @@
 /**
  * Created by АленкаиВова on 19.05.2016.
+ * @author vmetenev
+ * @version
+ * @since JDK1.6+
  */
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.vision.v1.Vision;
 import com.google.api.services.vision.v1.VisionScopes;
 import com.google.api.services.vision.v1.model.AnnotateImageRequest;
@@ -20,7 +21,6 @@ import com.google.api.services.vision.v1.model.Image;
 import com.google.common.collect.ImmutableList;
 
 import java.io.*;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,23 +28,18 @@ import java.security.GeneralSecurityException;
 import java.util.List;
 
 @SuppressWarnings("serial")
+
 public class GVisionQuickstart {
     /** Application name. */
+
     private static final String APPLICATION_NAME = "Robot_test_1";
-
     private static final String IMAGES_PATH = "IMAGES_PATH";
-
     private static final int MAX_LABELS = 3;
+    private final Vision vision;
 
-       /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
-
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
+    public GVisionQuickstart(Vision vision) {
+        this.vision = vision;
+    }
 
     public static void main(String[] args) throws IOException, GeneralSecurityException {
         if (args.length != 1) {
@@ -59,7 +54,8 @@ public class GVisionQuickstart {
 
         printLabels(System.out, imagePath, app.labelImage(imagePath, MAX_LABELS));
     }
-    public static void printLabels(PrintStream out, Path imagePath, List<EntityAnnotation> labels) {
+
+    private static void printLabels(PrintStream out, Path imagePath, List<EntityAnnotation> labels) {
         out.printf("Labels for image %s:\n", imagePath);
         for (EntityAnnotation label : labels) {
             out.printf(
@@ -72,8 +68,7 @@ public class GVisionQuickstart {
         }
     }
 
-
-    public static Vision getVisionService() throws IOException, GeneralSecurityException {
+    private static Vision getVisionService() throws IOException, GeneralSecurityException {
         GoogleCredential credential = GoogleCredential.getApplicationDefault().createScoped(VisionScopes.all());
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
         return new Vision.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, credential)
@@ -81,21 +76,11 @@ public class GVisionQuickstart {
                 .build();
     }
 
-    private final Vision vision;
-
-
-    public GVisionQuickstart(Vision vision) {
-        this.vision = vision;
-    }
-
-
-    /**
-     * Gets up to {@code maxResults} text annotations for images stored at {@code paths}.
-     */
-    public ImmutableList<EntityAnnotation> labelImage(Path path, int maxResults) {
+    public ImmutableList<EntityAnnotation> labelImage(Path path, int maxResults) throws IOException {
         ImmutableList.Builder<AnnotateImageRequest> requests = ImmutableList.builder();
         ListFilesUtils listFile = new ListFilesUtils(IMAGES_PATH);
         listFile.listFiles();
+
         try {
 
             for (int i = 0; i < listFile.listFileNames.size(); i++) {
@@ -111,7 +96,7 @@ public class GVisionQuickstart {
                                 .setFeatures(ImmutableList.of(
                                         new Feature()
                                                 .setType("LABEL_DETECTION")
-                                                .setMaxResults(MAX_LABELS))));
+                                                .setMaxResults(maxResults))));
             }
             long startTime = System.currentTimeMillis();
             Vision.Images.Annotate annotate =
@@ -119,33 +104,23 @@ public class GVisionQuickstart {
                             .annotate(new BatchAnnotateImagesRequest().setRequests(requests.build()));
             // Due to a bug: requests to Vision API containing large images fail when GZipped.
             annotate.setDisableGZipContent(true);
+
+            // [START parse_response]
             BatchAnnotateImagesResponse batchResponse = annotate.execute();
             assert batchResponse.getResponses().size() == listFile.listFileNames.size();
+
             long elapsedTime = System.currentTimeMillis() - startTime;
-
             System.out.println("Total elapsed http request/response time in milliseconds: " + elapsedTime);
-
             ImmutableList.Builder<EntityAnnotation> output = ImmutableList.builder();
             for (int i = 0; i < listFile.listFileNames.size(); i++) {
-                Path imagespath = Paths.get((String) listFile.listFileNames.get(i));
                 AnnotateImageResponse response = batchResponse.getResponses().get(i);
                 System.out.println(" "+response.getLabelAnnotations());
-             /*   output.add(
-                        labelImage.path(imagespath)
-                                .error(response.getError())
-                                .build());
-            */}
+            }
             return output.build();
         } catch (IOException ex) {
             // Got an exception, which means the whole batch had an error.
             ImmutableList.Builder<EntityAnnotation> output = ImmutableList.builder();
-            for (int i = 0; i < listFile.listFileNames.size(); i++) {
-                /*output.add(
-                        labelImage.builder()
-                                .path(path)
-                                .textAnnotations(ImmutableList.<EntityAnnotation>of())
-                                .build());
-           */ }
+
             return output.build();
         }
     }
