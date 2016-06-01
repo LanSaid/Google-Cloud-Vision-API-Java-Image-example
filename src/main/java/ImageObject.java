660d2377e2ef0d4c63686d43bd9d94891ee6cbad
vladinir <metenevvv@gmail.com>
2016-06-01 23:39:59 +0500

    add setter and getter ImageObject

--- src/main/java/ImageObject.java
+++ src/main/java/ImageObject.java
@@ -1,4 +1,7 @@
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by АленкаиВова on 30.05.2016.
@@ -11,7 +14,28 @@ public class ImageObject {
     private float x;
     private float y;
     private String imagePath;
-    private ArrayList listLabel;
+    private List<EntityAnnotation> listLabel;
+    private String Status;
+
+    public ImageObject(String ImagePath, String Status, List<EntityAnnotation> listLabel) {
+        this.imagePath = ImagePath;
+        this.Status = Status;
+        this.listLabel = listLabel;
+
+
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public List<EntityAnnotation> getListLabel() {
+        return listLabel;
+    }
+
+    public void setListLabel(List<EntityAnnotation> listLabel) {
+        this.listLabel = listLabel;
+    }
 
     public float getY() {
         return y;
@@ -33,17 +57,8 @@ public class ImageObject {
         return imagePath;
     }
 
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
 
-    public ArrayList getListLabel() {
-        return listLabel;
-    }
 
-    public void setListLabel(ArrayList listLabel) {
-        this.listLabel = listLabel;
-    }
 
 
 }
